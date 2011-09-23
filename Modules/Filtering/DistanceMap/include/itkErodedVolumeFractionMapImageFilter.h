65c47362e7aedb78fcaaa589d3b6b7532e305932
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2011-09-23 15:55:10 +0200

    ENH: add evf filter
    Change-Id: I3ba61faab8dcb234f3ce67ed609e098c61c319cf

---
+++ Modules/Filtering/DistanceMap/include/itkErodedVolumeFractionMapImageFilter.h
@@ -0,0 +1,119 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkErodedVolumeFractionMapImageFilter_h
+#define __itkErodedVolumeFractionMapImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkBarrier.h"
+
+namespace itk {
+
+/** \class ErodedVolumeFractionMapImageFilter
+ * \brief Compute the eroded volume fraction map from a distance map
+ *
+ *
+ *
+ * \author Gaetan Lehmann. Biologie du Developpement et de la Reproduction, INRA de Jouy-en-Josas, France.
+ *
+ * \sa SignedMaurerDistanceMapImageFilter, SignedDanielssonDistanceMapImageFilter
+ * \ingroup ImageEnhancement  MathematicalMorphologyImageFilters
+ * \ingroup ITKDistanceMap
+ */
+template<class TInputImage, class TOutputImage>
+class ITK_EXPORT ErodedVolumeFractionMapImageFilter :
+    public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ErodedVolumeFractionMapImageFilter            Self;
+  typedef ImageToImageFilter<TInputImage, TOutputImage> Superclass;
+  typedef SmartPointer<Self>                            Pointer;
+  typedef SmartPointer<const Self>                      ConstPointer;
+
+  /** Some convenient typedefs. */
+  typedef TInputImage  InputImageType;
+  typedef TOutputImage OutputImageType;
+
+  typedef typename InputImageType::Pointer        InputImagePointer;
+  typedef typename InputImageType::ConstPointer   InputImageConstPointer;
+  typedef typename InputImageType::RegionType     InputImageRegionType;
+  typedef typename InputImageType::PixelType      InputImagePixelType;
+
+  typedef typename OutputImageType::Pointer        OutputImagePointer;
+  typedef typename OutputImageType::ConstPointer   OutputImageConstPointer;
+  typedef typename OutputImageType::RegionType     OutputImageRegionType;
+  typedef typename OutputImageType::PixelType      OutputImagePixelType;
+
+  typedef typename OutputImageType::IndexType      IndexType;
+
+  /** ImageDimension constants */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+
+  /** Standard New method. */
+  itkNewMacro(Self);
+
+  /** Runtime information support. */
+  itkTypeMacro(ErodedVolumeFractionMapImageFilter,
+               ImageToImageFilter);
+
+
+protected:
+  ErodedVolumeFractionMapImageFilter();
+  ~ErodedVolumeFractionMapImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** ErodedVolumeFractionMapImageFilter needs to request enough of the
+   * marker image to account for the elementary structuring element.
+   * The mask image does not need to be padded. Depending on whether
+   * the filter is configured to run a single iteration or until
+   * convergence, this method may request all of the marker and mask
+   * image be provided. */
+  void GenerateInputRequestedRegion();
+
+  /** This filter will enlarge the output requested region to produce
+   * all of the output if the filter is configured to run to
+   * convergence.
+   * \sa ProcessObject::EnlargeOutputRequestedRegion() */
+  void EnlargeOutputRequestedRegion(DataObject *itkNotUsed(output));
+
+  void BeforeThreadedGenerateData(void);
+  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                            ThreadIdType threadId );
+  void AfterThreadedGenerateData(void);
+
+private:
+  ErodedVolumeFractionMapImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  typedef std::map< InputImagePixelType, unsigned long, std::greater< InputImagePixelType > > InMapType;
+  std::vector<InMapType>    m_InMaps;
+  // the map which will store the output histogram
+  typedef std::map< InputImagePixelType, OutputImagePixelType > OutMapType;
+  OutMapType                m_OutMap;
+  typename Barrier::Pointer m_Barrier;
+
+}; // end of class
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkErodedVolumeFractionMapImageFilter.hxx"
+#endif
+
+#endif
---
+++ Modules/Filtering/DistanceMap/include/itkErodedVolumeFractionMapImageFilter.hxx
@@ -0,0 +1,182 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkErodedVolumeFractionMapImageFilter.hxx,v $
+  Language:  C++
+  Date:      $Date: 2004/12/21 22:47:30 $
+  Version:   $Revision: 1.12 $
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+    This software is distributed WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+    PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkErodedVolumeFractionMapImageFilter_hxx
+#define __itkErodedVolumeFractionMapImageFilter_hxx
+
+#include "itkErodedVolumeFractionMapImageFilter.h"
+#include "itkProgressReporter.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkImageRegionIterator.h"
+
+namespace itk {
+
+template <class TInputImage, class TOutputImage>
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::ErodedVolumeFractionMapImageFilter()
+{
+}
+
+
+template <class TInputImage, class TOutputImage>
+void
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+
+  // get pointers to the inputs
+  InputImagePointer  inputPtr =
+    const_cast< InputImageType * >( this->GetInput() );
+
+  if ( !inputPtr )
+    { return; }
+
+  // We need to
+  // configure the inputs such that all the data is available.
+  //
+  inputPtr->SetRequestedRegion(inputPtr->GetLargestPossibleRegion());
+}
+
+
+template <class TInputImage, class TOutputImage>
+void
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::EnlargeOutputRequestedRegion(DataObject *)
+{
+  this->GetOutput()->SetRequestedRegion( this->GetOutput()->GetLargestPossibleRegion() );
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::BeforeThreadedGenerateData(void)
+{
+  typename TOutputImage::Pointer output = this->GetOutput();
+  typename TInputImage::ConstPointer input = this->GetInput();
+
+  long nbOfThreads = this->GetNumberOfThreads();
+  if( itk::MultiThreader::GetGlobalMaximumNumberOfThreads() != 0 )
+    {
+    nbOfThreads = vnl_math_min( this->GetNumberOfThreads(), itk::MultiThreader::GetGlobalMaximumNumberOfThreads() );
+    }
+  // number of threads can be constrained by the region size, so call the SplitRequestedRegion
+  // to get the real number of threads which will be used
+  typename TOutputImage::RegionType splitRegion;  // dummy region - just to call the following method
+  nbOfThreads = this->SplitRequestedRegion(0, nbOfThreads, splitRegion);
+
+  m_Barrier = Barrier::New();
+  m_Barrier->Initialize( nbOfThreads );
+  m_InMaps.clear();
+  m_InMaps.resize( nbOfThreads );
+}
+
+template<class TInputImage, class TOutputImage>
+void
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                       ThreadIdType threadId)
+{
+  // do not allocate the output now, to decrease the memory usage
+  ProgressReporter progress(this, threadId, outputRegionForThread.GetNumberOfPixels()*2);
+
+  InMapType & inMap = m_InMaps[threadId];
+
+  // fill the map
+  long inside = 0;
+  ImageRegionConstIterator< InputImageType > iIt( this->GetInput(), outputRegionForThread );
+  for( iIt.GoToBegin(); !iIt.IsAtEnd(); ++iIt )
+    {
+    const InputImagePixelType & v = iIt.Get();
+    inMap[ v ]++;
+    if( v > 0 )
+      {
+      inside++;
+      }
+    progress.CompletedPixel();
+    }
+
+  m_Barrier->Wait();
+
+  if( threadId == 0 )
+    {
+    m_OutMap.clear();
+    typename InMapType::const_iterator mapIt;
+
+    // group the histograms
+    for( unsigned int i=1; i<m_InMaps.size(); i++ )
+      {
+      InMapType & inMapT = m_InMaps[i];
+      for( mapIt = inMapT.begin(); mapIt != inMapT.end(); mapIt++ )
+        {
+        inMap[ mapIt->first ] += mapIt->second;
+        if( mapIt->first > 0 )
+          {
+          inside += mapIt->second;
+          }
+        }
+      }
+    // compute the density of the histogram
+    long nb = 0;
+    for( mapIt = inMap.begin(); mapIt != inMap.end(); mapIt++ )
+      {
+      m_OutMap[ mapIt->first ] = ( inside - nb ) / static_cast< OutputImagePixelType >( inside );
+      nb += mapIt->second;
+      }
+    this->AllocateOutputs();
+    }
+
+  m_Barrier->Wait();
+
+  // now clear the input map
+  inMap.clear();
+
+  // and fill the output image
+  ImageRegionIterator< OutputImageType > oIt( this->GetOutput(), outputRegionForThread );
+  for( iIt.GoToBegin(), oIt.GoToBegin();
+    !iIt.IsAtEnd();
+    ++iIt, ++oIt )
+    {
+    oIt.Set( m_OutMap[ iIt.Get() ] );
+    progress.CompletedPixel();
+    }
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::AfterThreadedGenerateData(void)
+{
+  m_InMaps.clear();
+  m_OutMap.clear();
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+ErodedVolumeFractionMapImageFilter<TInputImage, TOutputImage>
+::PrintSelf(std::ostream &os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+//   os << indent << "FullyConnected: "  << m_FullyConnected << std::endl;
+}
+
+}// end namespace itk
+#endif
